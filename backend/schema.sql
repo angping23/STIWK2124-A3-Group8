cfb1fcca6aedeaae4e62bac3f3d91f960e31d897
Tan Ying Ling <tan.ying.ling.20021128@gmail.com>
2026-06-21 05:54:00 +0800

    Add Docker and CI

--- backend/schema.sql
+++ backend/schema.sql
@@ -8,3 +8,11 @@ CREATE TABLE IF NOT EXISTS books (
     category VARCHAR(100) NOT NULL,
     description TEXT NOT NULL
 );
+INSERT INTO books (title, author, category, description) VALUES
+('Java Programming', 'John Doe', 'Education', 'Java book'),
+('Java Programming', 'John Doe', 'Education', 'Basic Java'),
+('Java Programming', 'John Doe', 'Education', 'Basic Java book'),
+('Java Programming', 'John Doe', 'Education', 'Basic Java book'),
+('Java Programming', 'John Doe', 'Education', 'Basic Java book'),
+('Java Programming', 'John Doe', 'Education', 'Basic Java book'),
+('Java Basics', 'John Lee', 'Programming', 'Beginner Java book');
